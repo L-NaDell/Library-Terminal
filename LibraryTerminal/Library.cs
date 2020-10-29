0cdf7a313b81c73c890f3bc956a51d6683acbab3
Robert Bizon <m4stersplint3r@gmail.com>
2020-10-29 14:33:51 -0400

    pre-merge from main

--- LibraryTerminal/Library.cs
+++ LibraryTerminal/Library.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LibraryTerminal
 {
     class Library
     {
+        public List<Media> LibraryStock { get; set; }
 
+        public Library() {
+            List<Media> booksFromFile = new List<Media>();
+            StreamReader read = new StreamReader("../../../Data/BookStock.txt");
+            string line = read.ReadLine();
+
+            while(line != null)
+            {
+                string[] bookProperties = line.Split("|");
+                booksFromFile.Add(new Book(
+                    bookProperties[0],
+                    )
+            }
+        }
+
+        List<Media> libraryStock = new List<Media>();
+
+        
     }
 }
